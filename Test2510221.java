16872e515afca1ac947d4ea72a9110535f9d7b27
resunrichard <116341273+resunrichard@users.noreply.github.com>
2022-11-11 13:54:17 +0530

    Add files via upload

---
+++ Test2510221.java
@@ -0,0 +1,9 @@
+class Test2510221{
+	public static void main(String args[]){
+		int i=2;
+		System.out.println("List of 10 even numbers\n");
+		do{
+			System.out.print(" "+i);
+		i=i+2;}while(i<=20);
+	}
+}
